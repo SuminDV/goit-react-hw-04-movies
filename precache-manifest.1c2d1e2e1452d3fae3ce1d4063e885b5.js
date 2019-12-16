ff1d8b86c831db041dea5783b1f21fde6d36cc8c
SuminDV <sumin.dmytro@gmail.com>
2019-12-16 12:32:54 +0200

    Updates

---
+++ precache-manifest.1c2d1e2e1452d3fae3ce1d4063e885b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679d68c27dc46724934f209c8603c1f2",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "5706c7ee1c5a9766117f",
+    "url": "/goit-react-hw-04-movies/static/css/main.fb93dcf9.chunk.css"
+  },
+  {
+    "revision": "ce27e9ceb8dbc29700af",
+    "url": "/goit-react-hw-04-movies/static/js/2.a9871a9e.chunk.js"
+  },
+  {
+    "revision": "5706c7ee1c5a9766117f",
+    "url": "/goit-react-hw-04-movies/static/js/main.de27d60d.chunk.js"
+  },
+  {
+    "revision": "be6d2349aaaae5a82c63",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.2adebc56.js"
+  }
+]);
